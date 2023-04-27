6ad581ff05adaa2c008f184e301837ac0a3a1271
Eliiss <92871000+Eliiss@users.noreply.github.com>
2023-04-27 09:22:25 +0200

    Add files via upload

---
+++ JavaEat.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaeat;
+
+/**
+ *
+ * @author elis8
+ */
+public class JavaEat {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+ 
